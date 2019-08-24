417c4f54bb02cfd61372c01d7ff5fe94a34a6d9b
nopplee <nopparutlee@gmail.com>
2019-08-24 18:19:33 +0700

    Updated full_system code

--- system/full_system/full_system.ino
+++ system/full_system/full_system.ino
@@ -36,14 +36,14 @@ float low_moisture =    30;
 #define oxygenPin       A2
 float low_O2 =          5.0;
 
-#define controllable_amount 2;   //number of environment measurement that can be controlled
+#define CONTROLLABLE_AMOUNT 2;   //number of environment measurement that can be controlled
 /*  pos           description
- *  0             Oxegen
+ *  0             Oxygen
  *  1             Soil moisture
  */
-bool lack_state[controllable_amount];
-//threshold values here
-int threshold_value[controllable_amount];
+bool lack_state[CONTROLLABLE_AMOUNT];
+//**** threshold_value [controller index] [lower/upper index (0 for lower, 1 for upper)]
+int threshold_value[CONTROLLABLE_AMOUNT][2] = {};   //threshold values here
 
 int Delay_Time = 0;
 int state = LOW;      // the system currently closed
@@ -71,7 +71,7 @@ void setup() {
   pinMode(Power_Air_Pump,  OUTPUT);
   pinMode(onPin,           INPUT);
   pinMode(offPin,          INPUT);
-  for(int i=0;i<controllable_amount;i++){
+  for(int i=0;i<CONTROLLABLE_AMOUNT;i++){
     lack_state[i] = false;
   }
 }
@@ -103,26 +103,49 @@ void loop() {
     //set lack O2 flag
 
   //write as a loop
-  for(int i = 0; i < controllable_amount ; i++){
-    //check if the flag is set
+  float packed_value[CONTROLLABLE_AMOUNT];
+  /*
+   * Please assign the read values into this array
+   * The order should follow the index in line (around) 40
+   */
+  for(int i = 0; i < CONTROLLABLE_AMOUNT ; i++){
+    int threshold = threshold_value[i][(lack_state[i]) ? 0 : 1];
+    //compare value from sensors, this comparison can be written in switch-cases manner or just an array index looping
+    //I do prefer array index loop though. - Lee
+    lack_state[i] = (packed_value[i] < threshold) ? true : false;
   }
 
   if (on_reading == LOW) {                // if on_button is toggled  
     if (state == LOW){                   // if system is closed
       state = HIGH;                        // open the system
-      //wateringPlant(true);                 // 
-      //Check LACK O2 FLAG
-      //if lack
-        //Open the air pump
-      //else
-        //Close the air pump
-      //Check LACK SOIL MOISTURE FLAG
-      //if lack
-        //Open solenoid2
-        //Open water pump
-      //else
-        //Close water pump
-        //Close solenoid2
+      for(int i=0;i<CONTROLLABLE_AMOUNT;i++){
+        if(lack_state[i]){
+          switch(i){ //if lack
+            case 0: //Oxygen
+              digitalWrite(Power_Air_Pump, HIGH); //open air pump
+              break;
+            case 1: //soil moisture
+              digitalWrite(Valve2, HIGH); //open solenoid2
+              delay(1000);
+              digitalWrite(Power_pump, HIGH); //open water pump
+              break;
+            default: Serial.print("stop the system as soon as possible and check the amount of controllable stuffs and the number of cases");
+          }
+        }
+        else{
+          switch(i){ //if lack
+            case 0: //Oxygen
+              digitalWrite(Power_Air_Pump, LOW); //close air pump
+              break;
+            case 1: //soil moisture
+              digitalWrite(Power_pump, LOW); //close water pump
+              delay(1000);
+              digitalWrite(Valve2, LOW); // close solenoid
+              break;
+            default: Serial.print("stop the system as soon as possible and check the amount of controllable stuffs and the number of cases");
+          }
+        }
+      }
     }
   }
   
