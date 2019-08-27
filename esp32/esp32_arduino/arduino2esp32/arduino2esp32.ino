9fa8955335cbca42dc29ccd5e2f75a87be233988
bundithou <bundit.hou@gmail.com>
2019-08-28 00:03:14 +0700

    update communication code (esp32 <--> arduino)

--- esp32/esp32_arduino/arduino2esp32/arduino2esp32.ino
+++ esp32/esp32_arduino/arduino2esp32/arduino2esp32.ino
@@ -9,7 +9,16 @@ void setup() {
 }
 
 void loop() {
-  int value = id*2;
+ if (sw.available() > 0) {
+   Serial.println("receive: ");
+   char bfr[501];
+   memset(bfr,0, 501);
+   sw.readBytesUntil( '\n',bfr,500);
+   Serial.println(bfr);
+ }
+ 
+ int value = id*2;
+ 
  Serial.println("Sending data to nodemcu");
  Serial.print("{\"sensorid\":");
  Serial.print(id);//sensor id
@@ -27,4 +36,5 @@ void loop() {
  sw.print("}");
  sw.println();
  delay(5000);
+ id +=1;
 }
--- esp32/esp32_arduino/esp322arduino/esp322arduino.ino
+++ esp32/esp32_arduino/esp322arduino/esp322arduino.ino
@@ -1,14 +1,20 @@
 
+HardwareSerial sw(2);
+
 void setup() {
   Serial.begin(115200); 
   Serial.println("setup");
+  sw.begin(115200); 
 }
 
 void loop() {
-   if (Serial.available() > 0) {
-     char bfr[501];
-     memset(bfr,0, 501);
-     Serial.readBytesUntil( '\n',bfr,500);
-     Serial.println(bfr);
-   }
+  String d = "";
+ if (sw.available() > 0) {
+   Serial.println("receive: ");
+   char bfr[501];
+   memset(bfr,0, 501);
+   sw.readBytesUntil( '\n',bfr,500);
+   Serial.println(bfr);
+ }
+   delay(5000);
  }
