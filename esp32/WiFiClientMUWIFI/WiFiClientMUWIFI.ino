7a1fb7f6bfeab29d16948ed5ba4b6c7308defd25
bundithou <bundit.hou@gmail.com>
2019-09-05 23:09:57 +0700

    update code -- failed to establish connection

--- esp32/WiFiClientMUWIFI/WiFiClientMUWIFI.ino
+++ esp32/WiFiClientMUWIFI/WiFiClientMUWIFI.ino
@@ -1,10 +1,32 @@
 #include <WiFi.h> //Wifi library
+#include <PubSubClient.h>
 #include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
 #define EAP_IDENTITY "u5988049" //if connecting from another corporation, use identity@organisation.domain in Eduroam 
 #define EAP_PASSWORD "dit18827" //your Eduroam password
 const char* ssid = "MU-WiFi"; // Eduroam SSID
-const char* host = "arduino.php5.sk"; //external server domain for HTTP connection after authentification
+//const char* host = "arduino.php5.sk"; //external server domain for HTTP connection after authentification
 int counter = 0;
+
+const char* mqtt_server = "35.198.234.67"; //<-- IP หรือ Domain ของ Server MQTT
+long lastMsg = 0;
+char msg[100];
+int value = 0;
+String DataString;
+
+WiFiClient espClient;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+PubSubClient client(mqtt_server, 1883, callback, espClient);
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -31,6 +53,29 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address set: "); 
   Serial.println(WiFi.localIP()); //print LAN IP
+
+  //client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("aquaponic", "hello world");
+      // ... and resubscribe
+      client.subscribe("command");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
 void loop() {
   if (WiFi.status() == WL_CONNECTED) { //if we are connected to Eduroam network
@@ -48,22 +93,17 @@ void loop() {
     ESP.restart();
     }
   }
-  Serial.print("Connecting to website: ");
-  Serial.println(host);
-  WiFiClient client;
-  if (client.connect(host, 80)) {
-    String url = "/rele/rele1.txt";
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: ESP32\r\n" + "Connection: close\r\n\r\n");
-
-    while (client.connected()) {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") {
-        break;
-      }
-    }
-    String line = client.readStringUntil('\n');
-   Serial.println(line);
-  }else{
-      Serial.println("Connection unsucessful");
-    }  
+  Serial.print("Connecting to : ");
+  Serial.println(mqtt_server);
+  //WiFiClient client;
+  if (!client.connected()) {
+    reconnect();
+  }
+  //if (client.connect("ESP32Client123456")) {
+  client.publish("aquaponic", "hello world meme1213");
+  //}else{
+   // Serial.println(client.state());
+   // Serial.println("Connection unsucessful");
+      
+  //}  
 }
