7ecb6594cac23401e9462f8120ad46a49ba43937
bundithou <bundit.hou@gmail.com>
2019-11-01 01:23:58 +0700

    add ESP32 with Arduino serial connect with MQTT

--- esp32/esp32_arduino/arduino2esp32/arduino2esp32.ino
+++ esp32/esp32_arduino/arduino2esp32/arduino2esp32.ino
@@ -4,9 +4,7 @@
   Tx1 Rx1
 
 */
-//#include <SoftwareSerial.h>
 
-//SoftwareSerial sw(2, 3); // RX, TX
 int id = 0;
 void setup() {
  Serial.begin(115200);
@@ -25,23 +23,6 @@ void loop() {
  }
  
  int value = id*2;
- 
-// Serial.println("Sending data to nodemcu");
-// Serial.print("{\"sensorid\":");
-// Serial.print(id);//sensor id
-// Serial.print(",");
-// Serial.print("\"value\":");
-// Serial.print(value);//offset
-// Serial.print("}");
-// Serial.println();
-// 
-// sw.print("{\"sensorid\":");
-// sw.print(id);//sensor id
-// sw.print(",");
-// sw.print("\"value\":");
-// sw.print(value);//offset
-// sw.print("}");
-  // send to esp
  Serial1.println(value);
  delay(5000);
  id +=1;
